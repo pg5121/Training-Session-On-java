a71457e8f362e9c611c84ee0849184d88f795770
pg5121 <parthgurav0642@gmail.com>
2024-06-11 15:07:17 +0530

    Add files via upload

---
+++ commone.java
@@ -0,0 +1,17 @@
+package comparth;
+
+public class commone {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int total = a+b;
+		System.out.println("addition: "+total);
+}
+	public static void sub(int a,int b) {
+		//int d=10;
+		//int e=20;
+		int Total=a-b;
+		System.out.println(Total);
+	}
+
+}
---
+++ commtwo.java
@@ -0,0 +1,15 @@
+package comparth;
+
+public class commtwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		commone cm=new commone();  // object orentation
+		commone.add();
+		commone.sub(12,10);
+
+
+	}
+
+}
